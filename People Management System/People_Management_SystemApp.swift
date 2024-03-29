2de57985c9c1f4e393b65b566426bf3c1361d836
meganchlin <80561906+meganchlin@users.noreply.github.com>
2024-03-29 11:22:39 -0400

    Initial Commit

---
+++ People Management System/People_Management_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  People_Management_SystemApp.swift
+//  People Management System
+//
+//  Created by Megan Lin on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct People_Management_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
